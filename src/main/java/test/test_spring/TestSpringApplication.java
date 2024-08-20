2effbb9db985afd07425cc1604178e2507d5a5e7
kdy-1210 <zxmzxm1324@gmail.com>
2024-08-20 16:53:13 +0900

    first commit

---
+++ src/main/java/test/test_spring/TestSpringApplication.java
@@ -0,0 +1,14 @@
+package test.test_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpringApplication.class, args);
+	}
+
+}
+//tomcat이라는 웹서버가 내장되어있음
