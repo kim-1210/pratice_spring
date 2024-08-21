3639c49bac053eec60f0d09c09a421fe4985eadd
KDY <140900130+kim-1210@users.noreply.github.com>
2024-08-21 20:32:42 +0900

    Controller 파일 생성

---
+++ src/main/java/test/test_spring/Controller/TestController.java
@@ -0,0 +1,15 @@
+package test.test_spring.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+    
+    @GetMapping("test")
+    public String test(Model model){
+        model.addAttribute("data", "test!!");
+        return "test"; //test.html을 찾음
+    }
+}
