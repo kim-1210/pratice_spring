2f4ef88ad3bc9dd3c0b781b37a0370b5f2e8152b
KDY <kdy@KDYui-MacBookAir.local>
2024-08-22 20:36:36 +0900

    spiring으로 가상 DB제작 및  api기능 작성

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 #pratice_spring
+
+### domain -> Member class가 들어있으며, long 타입의 id 와 String타입의 name 존재
+### repository -> Member Management API
+        - MemberRepository : 회원에 관한 api
+        - MemoryMemberRepository : api의 기능 작성
--- src/main/java/test/test_spring/Controller/TestController.java
+++ src/main/java/test/test_spring/Controller/TestController.java
@@ -3,6 +3,8 @@ package test.test_spring.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TestController {
@@ -11,5 +13,37 @@ public class TestController {
     public String test(Model model){
         model.addAttribute("data", "test!!");
         return "test"; //test.html을 찾음
+    }
+
+    @GetMapping("test-mvc")
+    public String testMVC(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name);
+        return "test-tem";
+    }
+
+    @GetMapping("test-string")
+    @ResponseBody //html의 부분을 template에서 가져오는 것이 아닌 java에서 그대로 사용
+    //http응답에 바로전달 함
+    //HttpMessageConverter가 동작함
+    public String testString(@RequestParam("name") String name){
+        return "test " + name;
+    }
+
+    @GetMapping("test-api")
+    @ResponseBody
+    public test testapi(@RequestParam("name") String name){ //api : 데이터만 전송하기 <json>
+        test TEST = new test();
+        TEST.setName(name);
+        return TEST;
+    }
+
+    static class test {
+        private String name;
+        public String getName(){
+            return this.name;
+        }
+        public void setName(String name){
+            this.name = name;
+        }
     }
 }
